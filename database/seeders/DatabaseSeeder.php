86cf26b2eefec61adcd4729f561fc94cea8846ea
ahmedalfaiq <ahmedalfaiq21@gmail.com>
2024-10-20 22:35:19 +0300

    update the Views and Database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Image;
+use App\Models\Product;
+
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,8 +20,34 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'admin ',
+            'email' => 'admin@example.com',
+            'password' => 'test1234',
+            'phone' => '589648',
+            'role' => 'admin',
+            'remember_token' => now(),
         ]);
+        Category::create(['name' => 'صندل ',  'image' => 'img2.jpg', 'type' => "Men"]);
+        Category::create(['name' => 'ثياب مطرزه ',  'image' => 'img3.jpg', 'type' => "Women"]);
+        Category::create(['name' => 'عسيب ',  'image' => 'img4.jpg', 'type' => "Accessory"]);
+        Category::create(['name' => 'كوفيه ',  'image' => 'img5.jpg', 'type' => "Accessory"]);
+
+
+        Product::create(['name' => 'Abaya 1', 'description' => 'Abaya 1 New Models',  'price' => '15', 'size' => 'S', 'category_id' => 1]);
+        Product::create(['name' => 'Abaya 2', 'description' => 'Abaya 2 New Models',  'price' => '8', 'size' => 'M',  'category_id' => 2]);
+        Product::create(['name' => 'Abaya 3', 'description' => 'Abaya 3 New Models',  'price' => '8', 'size' => 'L', 'category_id' => 3]);
+        Product::create(['name' => 'Abaya 4', 'description' => 'Abaya 5 New Models',  'price' => '8', 'size' => 'L', 'category_id' => 4]);
+
+        Image::create(['product_id'=>1,'image' => 'img5.jpg']);
+        Image::create(['product_id'=>1,'image' => 'img4.jpg']);
+        Image::create(['product_id'=>2,'image' => 'img3.jpg']);
+        Image::create(['product_id'=>2,'image' => 'img1.jpg']);
+        Image::create(['product_id'=>3,'image' => 'img5.jpg']);
+        Image::create(['product_id'=>3,'image' => 'img4.jpg']);
+        Image::create(['product_id'=>4,'image' => 'img3.jpg']);
+        Image::create(['product_id'=>4,'image' => 'img1.jpg']);
+
+
     }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,79 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminCategoryController;
+use App\Http\Controllers\AdminOrderController;
+use App\Http\Controllers\AdminProductController;
+use App\Http\Controllers\AdminUserController;
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\MyAccountController;
+use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
-    return view('welcome');
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/shop', [App\Http\Controllers\HomeController::class, 'shop'])->name('shop');
+Route::get('/cart', [App\Http\Controllers\HomeController::class, 'cart'])->name('cart');
+
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/category/{id}', [App\Http\Controllers\HomeController::class, 'show'])->name('products.show');
+Route::post('/products/men/search', [App\Http\Controllers\ProductController::class, 'search'])->name('products.men.search');
+Route::get('/products/type', [App\Http\Controllers\ProductController::class, 'select'])->name('products.type');
+Route::get('/men', [App\Http\Controllers\ProductController::class, 'index'])->name('men');
+Route::get('/women', [App\Http\Controllers\ProductController::class, 'women'])->name('women');
+Route::get('/accessory', [App\Http\Controllers\ProductController::class, 'accessory'])->name('accessory');
+Route::get('/products/{id}/show', [App\Http\Controllers\ProductController::class, 'show'])->name('product.women');
+Route::get('/products/detail/{id}', [App\Http\Controllers\ProductController::class, 'detail'])->name('detail');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::middleware('auth')->group(function () {
+
+    Route::get('/cart', [CartController::class, 'index'])->name("cart.index");
+    Route::get('/cart/delete', [CartController::class, 'delete'])->name("cart.delete");
+    Route::post('/cart/add/{id}', [CartController::class, 'add'])->name("cart.add");
+    Route::post('/product/rate', [ProductController::class, 'rate'])->name("product.rating");
+    Route::post('/my-account/orders/{id}/delivered', [MyAccountController::class, 'delivered'])->name("myaccount.delivered");
+    Route::get('/cart/purchase', [CartController::class, 'purchase'])->name("cart.purchase");
+    Route::get('/my-account/orders', [MyAccountController::class, 'orders'])->name("myaccount.orders");
 });
+
+Route::prefix('admin')->group(function () {
+    Route::get('/user', [AdminUserController::class, 'index'])->name("admin.users");
+    Route::delete('/users/{id}/delete', [AdminUserController::class, 'delete'])->name("admin.user.delete");
+    Route::post('/users/{id}/update', [AdminUserController::class, 'update'])->name("admin.user.update");
+    Route::get('/users/{id}/edit', [AdminUserController::class, 'edit'])->name("admin.user.edit");
+
+
+    Route::get('/product/{id}', [AdminProductController::class, 'show'])->name("admin.product.show");
+    Route::get('/product', [AdminProductController::class, 'index'])->name("admin.product.index");
+    Route::post('/product/store', [AdminProductController::class, 'store'])->name("admin.product.store");
+    Route::delete('/product/{id}/delete', [AdminProductController::class, 'delete'])->name("admin.product.delete");
+    Route::post('/product/{id}/update', [AdminProductController::class, 'update'])->name("admin.product.update");
+    Route::delete('/product/{id}/image/delete', [AdminProductController::class, 'deleteImage'])->name("admin.product.image.delete");
+    Route::post('/product/image', [AdminProductController::class, 'image'])->name("admin.product.image");
+
+    Route::get('/category/{id}', [AdminCategoryController::class, 'show'])->name("admin.category.show");
+    Route::get('/category', [AdminCategoryController::class, 'index'])->name("admin.category.index");
+    Route::post('/category/store', [AdminCategoryController::class, 'store'])->name("admin.category.store");
+    Route::delete('/category/{id}/delete', [AdminCategoryController::class, 'delete'])->name("admin.category.delete");
+    Route::post('/category/{id}/update', [AdminCategoryController::class, 'update'])->name("admin.category.update");
+
+    Route::get('/orders', [AdminOrderController::class, 'orders'])->name("admin.orders");
+    Route::get('/orders/{id}/delivered', [AdminOrderController::class, 'delivered'])->name("admin.delivered");
+
+
+});
+
+
